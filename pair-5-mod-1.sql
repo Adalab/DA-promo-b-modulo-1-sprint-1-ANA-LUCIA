f9c1f7be1c6bbeb7c7d3b15b2102a94646a6fcd0
Lucía Cernuda <luciacernuda@gmail.com>
2022-07-14 13:03:01 +0200

    22/07/14 Ejercicios sobre consultas básicas 2, falta el 8

---
+++ pair-5-mod-1.sql
@@ -0,0 +1,62 @@
+use northwind;
+
+-- EJERCICIO 1--
+SELECT product_id, product_name, unit_price
+FROM products
+LIMIT 10;
+
+-- EJERCICIO 2--
+
+SELECT product_id, product_name, unit_price
+FROM products
+ORDER BY product_id DESC
+LIMIT 10;
+
+-- EJERCICIO 3 --
+
+SELECT DISTINCT order_id
+FROM order_details;
+
+-- EJERCICIO 4 --
+
+SELECT DISTINCT order_id
+FROM order_details
+LIMIT 2;
+
+-- EJERCICIO 5 --
+
+SELECT order_id, unit_price * quantity AS importe_total
+FROM order_details
+ORDER BY importe_total DESC
+LIMIT 3;
+
+-- EJERCICIO 6 --
+
+SELECT order_id, unit_price * quantity AS importe_total
+FROM order_details
+ORDER BY importe_total
+LIMIT 5
+OFFSET 5;
+
+-- EJERCICIO 7 --
+
+SELECT category_name AS nombre_de_categoría
+FROM categories;
+
+-- EJERCICIO 8 --
+SELECT shipped_date  DATE_ADD(day, -5, shipped_date) AS fecha_retrasada
+FROM orders;
+
+-- EJERCICIO 9 --
+
+SELECT product_name, unit_price
+FROM products
+WHERE unit_price BETWEEN 15 AND 50;
+
+-- EJERCICIO 10 --
+
+SELECT product_name, unit_price
+FROM products
+WHERE unit_price IN (18, 19, 20)
+
+
